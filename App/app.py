42d9f2004070fc5a9976cc48c5f133ef283cd227
Saint Tarila-Brisbe <saintbriisbe@gmail.com>
2020-12-03 17:06:38 +0100

    Created HTML template

---
+++ App/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_world():
+    return "Hello World !!"
+
+app.debug = True
+
+if __name__ == '__main__':
+    app.run()
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -4,13 +4,24 @@ import dangerous_queries
 from colorama import init
 init()
 from colorama import Fore, Style
+import difflib
+
+# Package to check for close matches
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 reset_styles = Style.RESET_ALL
 red_text = Fore.RED
 green_text = Fore.GREEN
+orange_text = Fore.MAGENTA
 bright_text = Style.BRIGHT
 
-final_result = None
+obtained_dangerous_queries = []
+close_match_ratio = None
+highest_match = None
+obtained_ratios = []
 
 
 # Dictionaries Here...
@@ -48,8 +59,47 @@ def main_control():
                         query_input = str(input(f"    Good! Now, type any {db_version_value} query you know \n\n    ==>  "))
                             
                         for second_key, second_value in dangerous_queries_dict.items():
-                            if query_input in second_value:
-                                print(f""" \n   {red_text}{bright_text} |  {query_input}  | {reset_styles} is a very dangerous {db_version_value} query ! \n""")
+                            obtained_dangerous_queries.append(second_value)
+                            
+                            for i in obtained_dangerous_queries:
+                                all_queries_lower = i.lower()
+                                query_input_lower = query_input.lower()
+                                                                                        
+                                seq_match = SequenceMatcher(a= all_queries_lower, b = str(query_input_lower) )
+                                ratio = (seq_match.ratio())
+                                highest_match = get_close_matches(query_input, obtained_dangerous_queries)
+
+                                obtained_ratios.append(float(ratio))
+                                
+
+
+                            if (query_input in obtained_dangerous_queries):
+                                print(f""" \n   {red_text}{bright_text}   {query_input}   {reset_styles} is a DANGEROUS {db_version_value} query ! \n""")
+                                exit(0)
+                        
+                            elif (query_input not in obtained_dangerous_queries and float(max(obtained_ratios)) > 0.65):
+                               print(f""" \n   {orange_text}{bright_text}  {query_input}  {reset_styles} is potentially dangerous !""")
+                               print(f"""      It's closest match is -->  {red_text}{bright_text} {next(iter(highest_match))} {reset_styles}""")
+                               exit(0)
+                                    
+                                    
+                        
+                            elif (query_input not in obtained_dangerous_queries):
+                                print(f""" \n   {green_text}{bright_text}  {query_input}  {reset_styles} is either a SAFE {db_version_value} query or isn't a {db_version_value} query at all... \n""")
+                                exit(0)
+                            
+                            else:
+                                print('Blah')
+                         
+            
+    else:
+        print('Unknown Database !')
+        
+           
+
+                        
+                         
+                                
                                 
                             
 
